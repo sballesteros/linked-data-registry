4f4e3e11e1fc681b6269127e6637e896538f5ad0
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-04 18:59:05 -0500

    validate_doc_update using schema json (tv4)

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var ddoc = module.exports = {
   shows: require("./lib/shows"),
   lists: require("./lib/lists"),
   views: require("./lib/views"),
+  validate_doc_update: require("./lib/validate_doc_update.js"),
   language: "javascript"
 };
-
 
 for(var key in modules){
   ddoc[key] = modules[key];
