7133d4ddab8e27695ce12b373e3a210416b81efe
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-04 00:44:13 -0500

    latest and search

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var modules = require('./lib/modules');
+
 var ddoc = module.exports = {
   _id:'_design/registry',
   rewrites: require("./lib/rewrites"),
   shows: require("./lib/shows"),
+  lists: require("./lib/lists"),
   views: require("./lib/views"),
   language: "javascript"
 };
 
 
+for(var key in modules){
+  ddoc[key] = modules[key];
+}
