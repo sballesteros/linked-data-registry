6a973986a41a26d7a8f76ee76fef913442f841d9
sebastien ballesteros <sebastien.ballesteros@gmail.com>
2013-12-08 02:03:50 -0500

    sync

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ var stan = nano.db.use('stan');
 var _users = nano.db.use('_users');
 
 var server = http.createServer(function (req, res) {
-  
+
   var pathname = url.parse(req.url).pathname;
   if(pathname !== '/'){
     _fail(res, new Error('invalid roote'));
   } else if (req.method != 'POST') {
     _fail(res, new Error('need a POST request'));
-  } else {    
+  } else {
     req.setEncoding('utf8');
     var data = '';
     req.on('data', function(chunk){ data += chunk; });
@@ -24,14 +24,18 @@ var server = http.createServer(function (req, res) {
         return _fail(res, new Error('invalid data'));
       }
 
-      if( (! (('granter' in data) && ('granted' in data) && ('dpkgName' in data)) && !('_id' in data) ) ){
-        return _fail(res, new Error('invalid data'));        
+      if(!(('granter' in data) && ('granted' in data) && ('dpkgName' in data))){
+        return _fail(res, new Error('invalid data'));
       }
 
       if(data.granter === data.granted){ // => first maintainer => retrieve dpkg (with data._id) and check if dpkg.username === data.granter;
 
+        if(!('_id' in data)){
+          return _fail(res, new Error('invalid data'));
+        }
+
         stan.show('registry', 'firstUsername', data._id, function(err, dpkg) {
-          if (err) return _fail(res, err);          
+          if (err) return _fail(res, err);
           if( data.granter && (dpkg.username !== data.granter) ){
             return _fail(res, new Error('not allowed'), 403);
           }
@@ -41,6 +45,8 @@ var server = http.createServer(function (req, res) {
       } else { //not the first time, check if granter is a maintainter of data.dpkgname
 
         _users.show('maintainers', 'maintains', data.granter, function(err, maintains) {
+          if(err) return _fail(res, err);
+
           if(maintains.indexOf(data.granter) === -1){
             return _fail(res, new Error('not allowed'), 403);
           }
@@ -61,7 +67,7 @@ console.log('Server running at http://127.0.0.1:8000/');
 function _fail(response, err, code){
   var data = JSON.stringify({'error': err.message || ''});
   response.writeHead(code || 400, {'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(data)});
-  response.end(data);    
+  response.end(data);
 };
 
 function _grant(response, data){
@@ -69,6 +75,6 @@ function _grant(response, data){
     if(err) return _fail(response, err);
     body = JSON.stringify(body);
     response.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(body)});
-    response.end(body);         
+    response.end(body);
   });
 };
