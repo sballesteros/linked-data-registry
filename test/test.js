032e3ec841ec21af9d4ee39d24693d71f59eb2b3
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-18 14:28:09 -0500

    refactor tests

--- test/test.js
+++ test/test.js
@@ -47,191 +47,218 @@ var dpkg = {
 
 var maintainers = [{'name': 'user_a', 'email': 'user@domain.io'}, {'name': 'user_b','email': 'user@domain.io'}];
 
+function createFixture(done){
+  request.put({url: rurl('/adduser/user_a'), json: userData}, function(err, resp, body){
+    var userB = clone(userData);
+    userB.name = 'user_b';
+    request.put({url: rurl('/adduser/user_b'), json: userB}, function(err, resp, body){
+      var userC = clone(userData);
+      userC.name = 'user_c';
+      request.put({url: rurl('/adduser/user_c'), json: userC}, function(err, resp, body){
+
+        request.put( { url: rurl('/test-dpkg/0.0.0'), auth: {user:'user_a', pass: pass}, json: dpkg }, function(err, resp, body){
+          var mydpkg = clone(dpkg);
+          mydpkg.version = '0.0.1';
+          request.put( { url: rurl('/test-dpkg/0.0.1'), auth: {user:'user_a', pass: pass}, json: mydpkg }, function(err, resp, body){
+
+            request.post( {url: rurl('/owner/add'), auth: {user:'user_a', pass: pass},  json: {username: 'user_b', dpkgName: 'test-dpkg'}}, function(err, resp, body){
+              done();
+            });
+
+          });
+        });
+      });
+    });
+  });
+};
+
+function rmAll(done){
+  rm(_users, 'org.couchdb.user:user_a', function(){
+    rm(_users, 'org.couchdb.user:user_b', function(){
+      rm(_users, 'org.couchdb.user:user_c', function(){
+        rm(registry, 'test-dpkg@0.0.0', function(){
+          rm(registry, 'test-dpkg@0.0.1', function(){
+            rm(registry, 'test-dpkg@0.0.2', function(){
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+};
+
+
 describe('data-registry', function(){
 
-  beforeEach(function(done){
-    request.put({url: rurl('/adduser/user_a'), json: userData}, function(err, resp, body){
-      var userB = clone(userData);
-      userB.name = 'user_b';
-      request.put({url: rurl('/adduser/user_b'), json: userB}, function(err, resp, body){
-        var userC = clone(userData);
-        userC.name = 'user_c';
-        request.put({url: rurl('/adduser/user_c'), json: userC}, function(err, resp, body){
+  describe('no side effect', function(){
 
-          request.put( { url: rurl('/test-dpkg/0.0.0'), auth: {user:'user_a', pass: pass}, json: dpkg }, function(err, resp, body){
-            var mydpkg = clone(dpkg);
-            mydpkg.version = '0.0.1';
-            request.put( { url: rurl('/test-dpkg/0.0.1'), auth: {user:'user_a', pass: pass}, json: mydpkg }, function(err, resp, body){
-
-              request.post( {url: rurl('/owner/add'), auth: {user:'user_a', pass: pass},  json: {username: 'user_b', dpkgName: 'test-dpkg'}}, function(err, resp, body){
-                done();
-              });
-
-            });
-          });
-        });
-      });
+    before(function(done){
+      createFixture(done);
     });
-  });
-  
-  it('should have an user', function(done){
-    _users.get('org.couchdb.user:user_a', function(err, body){
-      assert.equal(body.name, userData.name);
-      done();
-    });
-  });
-
-  it('should have a dpkg', function(done){
-    registry.get('test-dpkg@0.0.0', function(err, body){
-      assert.equal(body._id, 'test-dpkg@0.0.0');      
-      done();
-    });
-  });
-
-  it('should retrieve dpkg with url instead of resources', function(done){
-    request(rurl('/test-dpkg/0.0.0'), function(err, resp, body){
-      var expected = clone(dpkg);
-      delete expected.resources[0].data;
-      delete expected.date;
-      expected.resources[0].url = 'http://localhost:3000/test-dpkg/0.0.0/inline'
-      assert.deepEqual(JSON.parse(body), expected);      
-      done();
-    });
-  });
-
-  it('should retrieve dpkg as is', function(done){
-    request(rurl('/test-dpkg/0.0.0?clone=true'), function(err, resp, body){
-      var expected = clone(dpkg);
-      delete expected.date;
-      assert.deepEqual(JSON.parse(body), expected);      
-      done();
-    });
-  });
-
-  it('should retrieve a resource', function(done){
-    request(rurl('/test-dpkg/0.0.0/inline'), function(err, resp, body){
-      assert.deepEqual(JSON.parse(body), dpkg.resources[0].data);      
-      done();
-    });
-  });
-
-  it('should retrieve all the versions of test-dpkg', function(done){
-    request(rurl('/test-dpkg'), function(err, resp, body){
-      assert.deepEqual(JSON.parse(body), ['0.0.0', '0.0.1']);      
-      done();
-    });
-  });
-
-  it('should retrieve the latest version of test-dpkg', function(done){
-    request(rurl('/test-dpkg/latest'), function(err, resp, body){
-      assert.equal(JSON.parse(body).version, '0.0.1');      
-      done();
-    });
-  });
-
-  it('user_a and user_b should be maintainers of test-dpkg', function(done){
-    request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
-      assert.deepEqual(JSON.parse(body), maintainers);      
-      done();
-    });
-  });
-
-  it('should not let user_a overwrite the dpkg', function(done){
-    request.put( { url: rurl('/test-dpkg/0.0.0'), auth: {user:'user_a', pass: pass}, json: dpkg }, function(err, resp, body){
-      assert.equal(resp.statusCode, 409);
-      done();
-    });
-  });
-
-  it('should not let user_c upgrade the dpkg', function(done){
-    var mydpkg = clone(dpkg);
-    mydpkg.version = '0.0.2';
-    request.put( { url: rurl('/test-dpkg/0.0.2'), auth: {user:'user_c', pass: pass}, json: mydpkg }, function(err, resp, body){
-      assert.equal(resp.statusCode, 403);
-      done();
-    });
-  });
-
-  it('should not let user_c delete the dpkg and remove test-dpkg from teh roles of user_a and user_b', function(done){
-    request.del( { url: rurl('/test-dpkg'), auth: {user:'user_c', pass: pass} }, function(err, resp, body){
-      assert.equal(resp.statusCode, 403);
-      request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
-        assert.deepEqual(JSON.parse(body), maintainers);       
+    
+    it('should have an user', function(done){
+      _users.get('org.couchdb.user:user_a', function(err, body){
+        assert.equal(body.name, userData.name);
         done();
       });
     });
-  });
 
-  it('should let user_a delete the dpkg and remove test-dpkg from the roles of user_a and user_b', function(done){
-    request.del( { url: rurl('/test-dpkg'), auth: {user:'user_a', pass: pass} }, function(err, resp, body){
-      assert.equal(resp.statusCode, 200);
-
-      request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
-        assert.equal(resp.statusCode, 404);       
+    it('should have a dpkg', function(done){
+      registry.get('test-dpkg@0.0.0', function(err, body){
+        assert.equal(body._id, 'test-dpkg@0.0.0');      
         done();
       });
-
     });
-  });
 
-  it('should not let user_c add itself to the maintainers of test-dpkg', function(done){
-    request.post( {url: rurl('/owner/add'), auth: {user:'user_c', pass: pass},  json: {username: 'user_c', dpkgName: 'test-dpkg'}}, function(err, resp, body){
-      assert.equal(resp.statusCode, 403);
-      done();
-    });   
-  });
+    it('should retrieve dpkg with url instead of resources', function(done){
+      request(rurl('/test-dpkg/0.0.0'), function(err, resp, body){
+        var expected = clone(dpkg);
+        delete expected.resources[0].data;
+        delete expected.date;
+        expected.resources[0].url = 'http://localhost:3000/test-dpkg/0.0.0/inline'
+        assert.deepEqual(JSON.parse(body), expected);      
+        done();
+      });
+    });
 
-  it('should let user_a add user_c as a maintainers of test-dpkg and then let user_c upgrade test-dpkg', function(done){
-    request.post( {url: rurl('/owner/add'), auth: {user:'user_a', pass: pass},  json: {username: 'user_c', dpkgName: 'test-dpkg'}}, function(err, resp, body){
-      assert.equal(resp.statusCode, 200);
+    it('should retrieve dpkg as is', function(done){
+      request(rurl('/test-dpkg/0.0.0?clone=true'), function(err, resp, body){
+        var expected = clone(dpkg);
+        delete expected.date;
+        assert.deepEqual(JSON.parse(body), expected);      
+        done();
+      });
+    });
+
+    it('should retrieve a resource', function(done){
+      request(rurl('/test-dpkg/0.0.0/inline'), function(err, resp, body){
+        assert.deepEqual(JSON.parse(body), dpkg.resources[0].data);      
+        done();
+      });
+    });
+
+    it('should retrieve all the versions of test-dpkg', function(done){
+      request(rurl('/test-dpkg'), function(err, resp, body){
+        assert.deepEqual(JSON.parse(body), ['0.0.0', '0.0.1']);      
+        done();
+      });
+    });
+
+    it('should retrieve the latest version of test-dpkg', function(done){
+      request(rurl('/test-dpkg/latest'), function(err, resp, body){
+        assert.equal(JSON.parse(body).version, '0.0.1');      
+        done();
+      });
+    });
+
+    it('user_a and user_b should be maintainers of test-dpkg', function(done){
       request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
-        var expected = clone(maintainers);
-        expected.push({name:'user_c', email:'user@domain.io'});
-        assert.deepEqual(JSON.parse(body), expected);       
+        assert.deepEqual(JSON.parse(body), maintainers);      
+        done();
+      });
+    });
 
-        var mydpkg = clone(dpkg);
-        mydpkg.version = '0.0.2';
-        request.put( { url: rurl('/test-dpkg/0.0.2'), auth: {user:'user_c', pass: pass}, json: mydpkg }, function(err, resp, body){
-          assert.equal(resp.statusCode, 201);
+    it('should not let user_a overwrite the dpkg', function(done){
+      request.put( { url: rurl('/test-dpkg/0.0.0'), auth: {user:'user_a', pass: pass}, json: dpkg }, function(err, resp, body){
+        assert.equal(resp.statusCode, 409);
+        done();
+      });
+    });
+
+    it('should not let user_c upgrade the dpkg', function(done){
+      var mydpkg = clone(dpkg);
+      mydpkg.version = '0.0.2';
+      request.put( { url: rurl('/test-dpkg/0.0.2'), auth: {user:'user_c', pass: pass}, json: mydpkg }, function(err, resp, body){
+        assert.equal(resp.statusCode, 403);
+        done();
+      });
+    });
+
+    it('should not let user_c delete the dpkg and remove test-dpkg from teh roles of user_a and user_b', function(done){
+      request.del( { url: rurl('/test-dpkg'), auth: {user:'user_c', pass: pass} }, function(err, resp, body){
+        assert.equal(resp.statusCode, 403);
+        request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
+          assert.deepEqual(JSON.parse(body), maintainers);       
           done();
         });
-        
-      });
-    });   
-  });
-
-  it('should not let user_c rm user_a from the maintainers of test-dpkg', function(done){
-    request.post( {url: rurl('/owner/rm'), auth: {user:'user_c', pass: pass},  json: {username: 'user_a', dpkgName: 'test-dpkg'}}, function(err, resp, body){
-      assert.equal(resp.statusCode, 403);
-      done();
-    });   
-  });
-
-  it('should let user_a rm user_b from the maintainers of test-dpkg', function(done){
-    request.post( {url: rurl('/owner/rm'), auth: {user:'user_a', pass: pass},  json: {username: 'user_b', dpkgName: 'test-dpkg'}}, function(err, resp, body){
-      assert.equal(resp.statusCode, 200);
-      request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
-        assert.deepEqual(JSON.parse(body), maintainers.slice(0,-1));       
-        done();
-      });
-    });   
-  });
-
-  afterEach(function(done){
-    rm(_users, 'org.couchdb.user:user_a', function(){
-      rm(_users, 'org.couchdb.user:user_b', function(){
-        rm(_users, 'org.couchdb.user:user_c', function(){
-          rm(registry, 'test-dpkg@0.0.0', function(){
-            rm(registry, 'test-dpkg@0.0.1', function(){
-              rm(registry, 'test-dpkg@0.0.2', function(){
-                done();
-              });
-            });
-          });
-        });
       });
     });
+
+    it('should not let user_c add itself to the maintainers of test-dpkg', function(done){
+      request.post( {url: rurl('/owner/add'), auth: {user:'user_c', pass: pass},  json: {username: 'user_c', dpkgName: 'test-dpkg'}}, function(err, resp, body){
+        assert.equal(resp.statusCode, 403);
+        done();
+      });   
+    });
+
+    it('should not let user_c rm user_a from the maintainers of test-dpkg', function(done){
+      request.post( {url: rurl('/owner/rm'), auth: {user:'user_c', pass: pass},  json: {username: 'user_a', dpkgName: 'test-dpkg'}}, function(err, resp, body){
+        assert.equal(resp.statusCode, 403);
+        done();
+      });   
+    });
+
+    after(function(done){
+      rmAll(done);
+    });
+
   });
+
+
+  describe('side effects', function(){
+
+    beforeEach(function(done){
+      createFixture(done);
+    });
+
+    it('should let user_a delete the dpkg and remove test-dpkg from the roles of user_a and user_b', function(done){
+      request.del( { url: rurl('/test-dpkg'), auth: {user:'user_a', pass: pass} }, function(err, resp, body){
+        assert.equal(resp.statusCode, 200);
+
+        request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
+          assert.equal(resp.statusCode, 404);       
+          done();
+        });
+
+      });
+    });
+
+    it('should let user_a add user_c as a maintainers of test-dpkg and then let user_c upgrade test-dpkg', function(done){
+      request.post( {url: rurl('/owner/add'), auth: {user:'user_a', pass: pass},  json: {username: 'user_c', dpkgName: 'test-dpkg'}}, function(err, resp, body){
+        assert.equal(resp.statusCode, 200);
+        request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
+          var expected = clone(maintainers);
+          expected.push({name:'user_c', email:'user@domain.io'});
+          assert.deepEqual(JSON.parse(body), expected);       
+
+          var mydpkg = clone(dpkg);
+          mydpkg.version = '0.0.2';
+          request.put( { url: rurl('/test-dpkg/0.0.2'), auth: {user:'user_c', pass: pass}, json: mydpkg }, function(err, resp, body){
+            assert.equal(resp.statusCode, 201);
+            done();
+          });
+          
+        });
+      });   
+    });
+
+    it('should let user_a rm user_b from the maintainers of test-dpkg', function(done){
+      request.post( {url: rurl('/owner/rm'), auth: {user:'user_a', pass: pass},  json: {username: 'user_b', dpkgName: 'test-dpkg'}}, function(err, resp, body){
+        assert.equal(resp.statusCode, 200);
+        request(rurl('/owner/ls/test-dpkg'), function(err, resp, body){
+          assert.deepEqual(JSON.parse(body), maintainers.slice(0,-1));       
+          done();
+        });
+      });   
+    });
+
+    afterEach(function(done){
+      rmAll(done);
+    });
+
+  });
+
 
 });
 
